e3a7ca65632ece5d0574d40d5832b07a892c2da1
Uditya Narayan Tiwari <uditmerit@gmail.com>
2026-06-29 22:33:00 +0530

    minor updates

--- 24 GenAI Project Chat With SQL DB/app.py
+++ 24 GenAI Project Chat With SQL DB/app.py
@@ -151,3 +151,4 @@ if user_query:
                 "content": answer,
             }
         )
+
--- 26 Implementing Youtube Video And Website Url Content Summarization GEN AI App/app.py
+++ 26 Implementing Youtube Video And Website Url Content Summarization GEN AI App/app.py
@@ -10,3 +10,22 @@ st.title("🦜LangChain: Summarize Text From YT or Websites")
 st.subheader("Summarize URL")
 
 
+with st.sidebar:
+    groq_api_key = st.text_input("Groq API Key", value="password")
+
+generic_url = st.text_input("URL", label_visibility="collapsed")
+
+if st.button("Summarize the content from YT or Website"):
+    if not groq_api_key.strip() or not generic_url.strip():
+        st.error("Please provide the information about the api key")
+elif not validators.url(generic_url):
+    st.error("Please enter a validator Url. It can may be a YT video URL or Website URL")
+
+else:
+    try:
+        with st.spinner("Waiting..."):
+            if "youtube.com" in generic_url:
+                loader = YoutubeLoader.from_youtube_url(generic_url, add_video_info=True)
+            else:
+                loader = UnstructuredURLLoader()
+                
